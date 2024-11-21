965f4be7d7438f9c32d56aa25080d8e70de0de8d
thisisolya <70409834+thisisolya@users.noreply.github.com>
2024-11-21 15:03:02 +0100

    rename name folder

--- src/containers/home-page/index.tsx
+++ src/containers/home-page/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { NavLink } from "react-router-dom"; 
+import { NavLink } from "react-router-dom";
 import PageContainer from "../../components/page-container";
-import Name from "../../components/name";
 import AnimatedShape from "../../components/animated-shape";
 import ContactInfo from "../../components/contact-info";
+import Name from "../../components/name";
 
 const HomePage: React.FC = () => {
   return (
